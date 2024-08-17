8acaa0c83eb3ab73db652ac5fb507d5df258dfc6
= <=>
2024-08-18 04:05:23 +0900

    fix: out vite-plugin-obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import obfuscatorPlugin from 'vite-plugin-obfuscator'
+// import obfuscatorPlugin from 'vite-plugin-obfuscator'
 
 export default defineConfig({
   plugins: [
     react(),
-    obfuscatorPlugin({
-      globalOptions: {
-        debugProtection: true,
-      }
-    }),
+    // obfuscatorPlugin({
+    //   globalOptions: {
+    //     debugProtection: true,
+    //   }
+    // }),
   ],
   base: '/striker-base/',
 })
