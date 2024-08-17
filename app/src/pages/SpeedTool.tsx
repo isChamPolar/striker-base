df89777b81edf4830113a26b4560f899a549dda2
= <=>
2024-08-18 02:04:28 +0900

    feat: いったんコミット

--- app/src/pages/SpeedTool.tsx
+++ app/src/pages/SpeedTool.tsx
@@ -1,11 +1,81 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const SpeedTool = () => {
+  const handleClick = () => {console.log('clicked')};
   return (
-    <Box p={4}>
-      <Heading>スピード調整ツール</Heading>
-      {/* 詳細は後ほど実装 */}
-    </Box>
+    <Box
+        borderWidth="2px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        p={6}
+        m="10px auto"
+        maxW={{ base: "95%", md: "70%", lg: "70%" }} // 画面幅に応じて幅を設定
+        as="form"
+    >
+      <Heading
+        px={6}
+        mb={5}
+        fontWeight={600}
+        textAlign={'center'}
+        fontSize={{ base: '3xl', sm: '3xl', md: '4xl' }} // モバイル時に少し大きく
+        lineHeight={{ base: '160%', sm: '150%' }} // モバイル時に行間を少し広く
+      >
+        スピード調整ツール
+      </Heading>
+      {/* <Flex>
+        <FormControl mr="5%">
+          <FormLabel htmlFor="first-name" fontWeight={'normal'}>
+            First name
+          </FormLabel>
+          <Input id="first-name" placeholder="First name" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="last-name" fontWeight={'normal'}>
+            Last name
+          </FormLabel>
+          <Input id="last-name" placeholder="First name" />
+        </FormControl>
+      </Flex>
+      <FormControl mt="2%">
+        <FormLabel htmlFor="email" fontWeight={'normal'}>
+          Email address
+        </FormLabel>
+        <Input id="email" type="email" />
+        <FormHelperText>We&apos;ll never share your email.</FormHelperText>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
+          Password
+        </FormLabel>
+        <InputGroup size="md">
+          <Input
+            pr="4.5rem"
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+        <ButtonGroup mt="5%" w="100%">
+          <Flex w="100%" justifyContent="space-between">
+            <Flex>
+              <Button
+                isDisabled={false}
+                colorScheme="teal"
+                variant="solid"
+                w="7rem"
+                mr="5%">
+                Back
+              </Button>
+            </Flex>
+          </Flex>
+        </ButtonGroup> */}
+      </Box>
   );
 };
 
