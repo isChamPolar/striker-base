b43fc835b3cb82241dca4df787e91af370e5cc68
= <=>
2024-08-18 03:58:26 +0900

    feat:deploy

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscatorPlugin from 'vite-plugin-obfuscator'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscatorPlugin({
+      globalOptions: {
+        debugProtection: true,
+      }
+    }),
+  ],
+  base: '/striker-base/',
 })
