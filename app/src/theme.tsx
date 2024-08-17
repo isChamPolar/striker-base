e253701894276a14a94a4832993670aeabe556b2
= <=>
2024-08-18 00:29:08 +0900

    add: フォントの明示

--- app/src/theme.tsx
+++ app/src/theme.tsx
@@ -9,6 +9,10 @@ const theme = extendTheme({
       },
     },
   },
+  fonts: {
+    heading: `'Noto Sans JP', sans-serif`,
+    body: `'Noto Sans JP', sans-serif`,
+  },
 });
 
 export default theme;
