cde6402d7772c5125da9025de65e0fb58d8288b7
= <=>
2024-08-18 21:00:39 +0900

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import obfuscatorPlugin from 'vite-plugin-obfuscator'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    // obfuscatorPlugin({
-    //   globalOptions: {
-    //     debugProtection: true,
-    //   }
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'STRIKER BASE',
+        name: 'STRIKER BASE - モンスト便利ツール',
+        icons: [
+          {
+            src: '/striker-base-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '/',
+        background_color: '#ffffff',
+        display: 'standalone',
+        theme_color: '#000000',
+        description: 'STRIKER BASEは、モンストの攻略をサポートする便利ツールを提供します。',
+        orientation: 'portrait-primary',
+      },
+    }),
   ],
-})
+});
