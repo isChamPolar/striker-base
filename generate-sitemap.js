bf88759512ca7e87e8e778c8ddf0856e0c34a2fa
= <=>
2024-08-24 21:22:30 +0900

    test

---
+++ generate-sitemap.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const paths = [
+  '/',
+  '/about',
+  '/privacy-policy',
+  '/release-notes',
+  '/speed-tool',
+  '/exp-calculator',
+];
+
+const baseUrl = 'https://striker-base.com';
+const sitemapContent = `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${paths.map(path => `
+  <url>
+    <loc>${baseUrl}${path}</loc>
+  </url>
+  `).join('')}
+</urlset>
+`;
+
+fs.writeFileSync('./dist/sitemap.xml', sitemapContent.trim());
+console.log('✅ Sitemap has been generated!');
