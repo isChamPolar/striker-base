1f60b1a04605a19b785faaac576fe6960fdae7be
= <=>
2024-08-24 19:14:14 +0900

    fix: 08/24

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -71,8 +71,26 @@ export default function SmallCentered() {
           align={{ base: 'center', md: 'center' }}
           fontSize={12}
         >
-          <Text>モンスターストライクはXFLAG様が配信するiOS・Android用ゲームアプリです。</Text>
+          <Text>モンスターストライクは株式会社MIXI様が配信するiOS・Android用ゲームアプリです。</Text>
           <Link href='https://www.monster-strike.com/'>▶モンスターストライク公式サイト</Link>
+        </Container>
+      </Box>
+      <Box
+        borderTopWidth={1}
+        borderStyle={'solid'}
+        borderColor={'gray.200'}>
+        <Container
+          as={Stack}
+          maxW={'6xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ base: 'center', md: 'space-between' }}
+          align={{ base: 'center', md: 'center' }}
+          fontSize={12}
+        >
+          <Text>本アプリケーションは、モンスターストライクの非公式ツールとして開発されており、株式会社MIXI様および関連会社とは一切関係ありません。</Text>
+          <Link href='/about'>詳細はこちら</Link>
         </Container>
       </Box>
     </Box>
