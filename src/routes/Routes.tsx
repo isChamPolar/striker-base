7ba296c69f0260ef68c562bfcd8ac6187c166ec7
= <=>
2024-09-05 21:11:18 +0900

    bugfix

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,42 +1,24 @@
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import SpeedTool from '../pages/SpeedTool';
 import ExpCalculator from '../pages/ExpCalculator';
 import { About } from '../pages/About';
 import { Policy } from '../pages/Policy';
 import ReleaseNotesPage from '../pages/ReleaseNotesPage';
-// import ExpAdjuster from '../pages/ExpAdjuster';
-// import ExCounter from '../pages/ExCounter';
 import useGA4 from '../hooks/useGA4';
 
 const AppRoutes = () => {
   useGA4();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const path = new URLSearchParams(location.search).get('path');
-    
-    const nonRedirectPaths = ['/sitemap.xml', '/robots.txt', '/ads.txt'];
-
-    if (path && !nonRedirectPaths.some(nonRedirectPath => location.pathname.includes(nonRedirectPath))) {
-      navigate(path, { replace: true });
-    }
-  }, [location.search, location.pathname, navigate]);
-
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/privacy-policy" element={<Policy />} />
-      <Route path="/release-notes" element={<ReleaseNotesPage />} />
-      <Route path="/speed-tool" element={<SpeedTool />} />
-      <Route path="/exp-calculator" element={<ExpCalculator />} />
-      {/* <Route path="/exp-adjuster" element={<ExpAdjuster />} />
-      <Route path="/ex-counter" element={<ExCounter />} /> */}
-      <Route path="*" element={<HomePage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy-policy" element={<Policy />} />
+        <Route path="/release-notes" element={<ReleaseNotesPage />} />
+        <Route path="/speed-tool" element={<SpeedTool />} />
+        <Route path="/exp-calculator" element={<ExpCalculator />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
   );
 };
 
